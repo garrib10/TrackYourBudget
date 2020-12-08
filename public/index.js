91b4651e3bfe96722ff955aa7acb63c5b667caa2
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-08 12:55:50 -0500

    added service worker.js and packages and did the test offline and it worked

--- public/index.js
+++ public/index.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator){
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(reg => {
+      console.log("serviceWorker register" + reg) 
+    })
+  })
+}
+
+
 let transactions = [];
 let myChart;
 
---
+++ public/service-worker.js
@@ -0,0 +1,75 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+const DATA_CACHE_NAME = 'data-cache-v1'
+
+const FILES_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/index.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+ 
+    
+  ];
+  
+  self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+            return fetch (event.request).then(response => {
+                if (response.status === 200) {
+                    cache.put(event.request.url, response.clone())
+                }
+                return response
+            }).catch(err => {
+                return cache.match(event.request)
+            });
+        }).catch(err => console.log(err))
+    );
+    return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(() => {
+           return caches.match(event.request).then(response => {
+               if (response) {
+                   return response
+               }
+               else if (event.request.headers.get("accept").includes("text/html")) {
+                   return caches.match("/")
+               }
+           });
+        })
+    )
+  })
+         
