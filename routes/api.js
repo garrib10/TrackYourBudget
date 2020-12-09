85828a27d7c368b2950e6de25e7c755b334cb94b
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-08 23:01:54 -0500

    added lite-server

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
-const app = require("express").Router();
+const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-app.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -11,7 +11,7 @@ app.post("/api/transaction", ({body}, res) => {
     });
 });
 
-app.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -21,7 +21,7 @@ app.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-app.get("/api/transaction", (req, res) => {
+router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
@@ -31,4 +31,4 @@ app.get("/api/transaction", (req, res) => {
     });
 });
 
-module.exports = app;
+module.exports = router;
